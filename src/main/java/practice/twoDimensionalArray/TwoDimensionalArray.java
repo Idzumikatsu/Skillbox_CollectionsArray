fc89a8aea95dec99606abb6ab72cb997f3f75ea4
k0na <pwpvpdru@yandex.ru>
2023-06-04 17:13:18 +0300

    lesson_10.6 homework done - realize new methods in Hospital.java - realize new methods in ReverseArray.java - realize new methods in TwoDimensionalArray.java - cleanup, optimize and refactor some code

--- src/main/java/practice/twoDimensionalArray/TwoDimensionalArray.java
+++ src/main/java/practice/twoDimensionalArray/TwoDimensionalArray.java
@@ -1,17 +1,23 @@
 package practice.twoDimensionalArray;
 
+import java.util.Arrays;
+
 public class TwoDimensionalArray {
 
     public static final char SYMBOL = 'X';
 
     public static char[][] getTwoDimensionalArray(int size) {
 
-        //TODO: Написать метод, который создаст двумерный массив char заданного размера.
-        // массив должен содержать символ SYMBOL по диагоналям, пример для size = 3
-        // [X,  , X]
-        // [ , X,  ]
-        // [X,  , X]
+        char[][] arr = new char[size][size];
 
-        return new char[0][0];
+        for (char[] chars : arr) {
+            Arrays.fill(chars, ' ');
+        }
+        
+        for (int i = 0, j = 0, k = arr[j].length - 1 - j; i < arr.length; j++, k--, i++) {
+            arr[i][j] = SYMBOL;
+            arr[i][k] = SYMBOL;
+        }
+        return arr;
     }
 }
